1db328e5936c29d475ce7ebe4606109bf81922b1
Shreejai Raj <shreejairaj@gmail.com>
2024-06-12 13:41:32 +1000

    padination

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -16,21 +16,25 @@ const Test: React.FC = () => {
   const [properties, setProperties] = useState<Property[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [totalPages, setTotalPages] = useState<number>(1);
+  const limit = 5;
 
   useEffect(() => {
     const fetchProperties = async () => {
       try {
-        const res = await fetch('/api/properties');
+        const res = await fetch(`/api/properties?page=${currentPage}&limit=${limit}`);
         if (!res.ok) {
           throw new Error('Network response was not ok');
         }
         const data = await res.json();
         console.log("Fetched properties:", data); // Log fetched data
         
-        if (Array.isArray(data) && data.length === 0) {
+        if (Array.isArray(data.properties) && data.properties.length === 0) {
           setError("No properties found");
         } else {
-          setProperties(data);
+          setProperties(data.properties);
+          setTotalPages(data.totalPages);
         }
         setLoading(false);
       } catch (error) {
@@ -41,7 +45,15 @@ const Test: React.FC = () => {
     };
 
     fetchProperties();
-  }, []);
+  }, [currentPage]);
+
+  const handlePreviousPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+  };
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
@@ -54,7 +66,6 @@ const Test: React.FC = () => {
         {properties.map(property => (
           <li key={property.id} className='p-8 max-w-[900px] mx-auto'>
             <PropertyCard 
-              id={property.id}
               title={property.title} 
               beds={property.beds}
               bath={property.bath}
@@ -63,6 +74,11 @@ const Test: React.FC = () => {
           </li>
         ))}
       </ul>
+      <div className="pagination w-fit mx-auto flex gap-4 mb-4">
+        <button onClick={handlePreviousPage} disabled={currentPage === 1} className={` px-4 py-2 text-white rounded-lg ${currentPage === 1 ? 'bg-slate-500' : 'bg-orange-400'}`}>Previous</button>
+        <span className='py-2 px-4'>Page {currentPage} of {totalPages}</span>
+        <button onClick={handleNextPage} disabled={currentPage === totalPages} className={` px-4 py-2 text-white rounded-lg ${currentPage === totalPages ? 'bg-slate-500' : 'bg-orange-400'}`}>Next</button>
+      </div>
       <Footer />
     </>
   );
