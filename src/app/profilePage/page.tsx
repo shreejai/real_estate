3ebe529636b462405c62c44b74ef79adfae01747
Shreejai Raj <shreejairaj@gmail.com>
2024-06-23 12:26:09 +1000

    chat ui

--- src/app/profilePage/page.tsx
+++ src/app/profilePage/page.tsx
@@ -4,12 +4,13 @@ import Navbar from '@/components/Navbar'
 import Image from 'next/image'
 import React from 'react'
 import '@/app/profilePage/profilePage.css';
+import Chat from '@/components/chat/Chat'
 
 const page = () => {
   return (
     <div>
       <Navbar/>
-      <div className='profilePage grid grid-cols-2'>
+      <div className='profilePage grid grid-cols-1 md:grid-cols-2'>
         <div className="details p-4 pb-10">
           <div className="wrapper flex flex-col gap-8">
             <div className="title flex justify-between">
@@ -32,9 +33,9 @@ const page = () => {
             <List/>
           </div>
         </div>
-        <div className="chatContainer p-4">
+        <div className="chatContainer p-4 ">
           <div className="wrapper">
-            Chat
+            <Chat/>
           </div>
         </div>
       </div>
